8c41f44e2bf9ca80fc538896360f6b3cc2fd75f6
Châu Hoàng Phúc <144977826+chauhoangphuc05@users.noreply.github.com>
2025-03-12 12:08:51 +0700

    vệt bánh xe 2

--- Assets/Scripts/SmokeHandler.cs
+++ Assets/Scripts/SmokeHandler.cs
@@ -1,29 +1,27 @@
+using System.Diagnostics;
 using UnityEngine;
 
 public class SmokeHandler : MonoBehaviour
 {
+    public bool isOverpassEmitter = false;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
 
     TDCController TDCController;
     TrailRenderer trailRenderer;
-    public bool isOverpassEmitter = false;
     CarLayerHandler carLayerHandler;
     private void Awake()
     {
         TDCController = GetComponentInParent<TDCController>();
+        carLayerHandler = GetComponentInParent<CarLayerHandler>();
         trailRenderer = GetComponent<TrailRenderer>();
         trailRenderer.emitting = false;
-        carLayerHandler = GetComponentInParent<CarLayerHandler>();
-    }
-
-    void Start()
-    {
-        
+    
     }
 
     // Update is called once per frame
     void Update()
     {
+        trailRenderer.emitting = false;
         if (TDCController.isTireScreeching(out float lateralVelocity, out bool isBraking))
         {
             if (carLayerHandler.IsDrivingOnOverPass() && isOverpassEmitter)
@@ -35,6 +33,5 @@ public class SmokeHandler : MonoBehaviour
                 trailRenderer.emitting = true;
             }
         }
-
     }
 }
