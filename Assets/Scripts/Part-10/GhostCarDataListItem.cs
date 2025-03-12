80a23d453c659764fec782546d13f78c59849d53
rizy44 <tommytran1312@gmail.com>
2025-03-12 08:50:05 +0700

    Create function for ghost car, for replays

---
+++ Assets/Scripts/Part-10/GhostCarDataListItem.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[System.Serializable]
+// GhostCarDataListItem class
+public class GhostCarDataListItem : MonoBehaviour
+{
+
+	//start is call first before the first frame update
+	void Start()
+	{
+
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+	}
+
+}
