f2d1b1f550fc0b2c7cce2511338bfe1941b092f0
Châu Hoàng Phúc <144977826+chauhoangphuc05@users.noreply.github.com>
2025-03-20 22:58:42 +0700

    update menu

--- Assets/Menu/MainMenu.cs
+++ Assets/Menu/MainMenu.cs
@@ -11,4 +11,10 @@ public class MainMenu : MonoBehaviour
     {
         SceneManager.LoadScene("LocalPlay");
     }
+
+    public void QuitGame()
+    {
+        Debug.Log("QUIT!");
+        Application.Quit();
+    }
 }
