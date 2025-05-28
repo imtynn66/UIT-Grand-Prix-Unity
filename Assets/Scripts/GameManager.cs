a70eac0c5d29166118fbcc43dee5bf08b4ce3d01
Châu Hoàng Phúc <144977826+chauhoangphuc05@users.noreply.github.com>
2025-05-29 03:03:27 +0700

    ui/ux online game

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,17 @@
+﻿using Photon.Pun;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public GameObject playerPrefab;
+    public Transform[] spawnPoints; // 2 điểm spawn
+
+    void Start()
+    {
+        int actorIndex = PhotonNetwork.LocalPlayer.ActorNumber - 1; // ActorNumber bắt đầu từ 1
+        actorIndex = Mathf.Clamp(actorIndex, 0, spawnPoints.Length - 1);
+
+        Vector3 spawnPos = spawnPoints[actorIndex].position;
+        PhotonNetwork.Instantiate(playerPrefab.name, spawnPos, Quaternion.identity);
+    }
+}
