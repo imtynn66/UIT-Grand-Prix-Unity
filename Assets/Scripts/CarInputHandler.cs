e011e4b131d22e4e55d1ef2eb88d4a1b0e8fc5e6
Châu Hoàng Phúc <144977826+chauhoangphuc05@users.noreply.github.com>
2025-05-28 08:55:38 +0700

    leaderboard

--- Assets/Scripts/CarInputHandler.cs
+++ Assets/Scripts/CarInputHandler.cs
@@ -1,44 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class CarInputHandler : MonoBehaviour
 {
-    // Reference to TDCController to set the input vector for the car
-    TDCController TDCController;
+    public int playerNumber = 1;
 
-    // Ki?u input mà xe s? s? d?ng (phím m?i tên ho?c WASD)
-    public enum ControlType
-    {
-        ArrowKeys,
-        WASD
-    }
+    //Components
+    TDCController topDownCarController;
 
-    public ControlType controlType = ControlType.ArrowKeys;
-
+    //Awake is called when the script instance is being loaded.
     void Awake()
     {
-        TDCController = GetComponent<TDCController>();
+        topDownCarController = GetComponent<TDCController>();
     }
 
-    // Update is called once per frame
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame and is frame dependent
     void Update()
     {
         Vector2 inputVector = Vector2.zero;
 
-        // Ki?m tra ki?u ði?u khi?n và áp d?ng input týõng ?ng
-        if (controlType == ControlType.ArrowKeys)
+        switch (playerNumber)
         {
-            // Ði?u khi?n b?ng phím m?i tên
-            inputVector.x = Input.GetKey(KeyCode.RightArrow) ? 1 : (Input.GetKey(KeyCode.LeftArrow) ? -1 : 0);
-            inputVector.y = Input.GetKey(KeyCode.UpArrow) ? 1 : (Input.GetKey(KeyCode.DownArrow) ? -1 : 0);
-        }
-        else if (controlType == ControlType.WASD)
-        {
-            // Ði?u khi?n b?ng phím WASD
-            inputVector.x = Input.GetKey(KeyCode.D) ? 1 : (Input.GetKey(KeyCode.A) ? -1 : 0);
-            inputVector.y = Input.GetKey(KeyCode.W) ? 1 : (Input.GetKey(KeyCode.S) ? -1 : 0);
+            case 1: // WASD
+                inputVector.x = (Input.GetKey(KeyCode.D) ? 1 : 0) - (Input.GetKey(KeyCode.A) ? 1 : 0);
+                inputVector.y = (Input.GetKey(KeyCode.W) ? 1 : 0) - (Input.GetKey(KeyCode.S) ? 1 : 0);
+                break;
+
+            case 2: // Arrow keys
+                inputVector.x = (Input.GetKey(KeyCode.RightArrow) ? 1 : 0) - (Input.GetKey(KeyCode.LeftArrow) ? 1 : 0);
+                inputVector.y = (Input.GetKey(KeyCode.UpArrow) ? 1 : 0) - (Input.GetKey(KeyCode.DownArrow) ? 1 : 0);
+                break;
         }
 
-        // G?i input cho TDCController
-        TDCController.SetInputVector(inputVector);
+        //Send the input to the car controller.
+        topDownCarController.SetInputVector(inputVector);
     }
 }
